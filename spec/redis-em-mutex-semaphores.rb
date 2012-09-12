6ed200a487a0314df3d0f7c60a84ec575970c09c
Rafał Michalski <royaltm75@gmail.com>
2012-09-12 15:50:04 +0200

    spec: catch exceptions in other fibers

--- spec/redis-em-mutex-semaphores.rb
+++ spec/redis-em-mutex-semaphores.rb
@@ -7,170 +7,168 @@ require 'redis-em-mutex'
 describe Redis::EM::Mutex do
 
   it "should lock and prevent locking on the same semaphore" do
-    begin
-      described_class.new(@lock_names.first).owned?.should be_false
-      mutex = described_class.lock(@lock_names.first)
-      mutex.names.should eq [@lock_names.first]
-      mutex.locked?.should be_true
-      mutex.owned?.should be_true
-      mutex.should be_an_instance_of described_class
-      described_class.new(@lock_names.first).try_lock.should be_false
-      expect {
-        mutex.lock
-      }.to raise_error(Redis::EM::Mutex::MutexError, /deadlock; recursive locking/)
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.locked?.should be_false
-      mutex.owned?.should be_false
-      mutex.try_lock.should be_true
-    ensure
-      mutex.unlock if mutex
-    end
+    described_class.new(@lock_names.first).owned?.should be_false
+    mutex = described_class.lock(@lock_names.first)
+    mutex.names.should eq [@lock_names.first]
+    mutex.locked?.should be_true
+    mutex.owned?.should be_true
+    mutex.should be_an_instance_of described_class
+    described_class.new(@lock_names.first).try_lock.should be_false
+    expect {
+      mutex.lock
+    }.to raise_error(Redis::EM::Mutex::MutexError, /deadlock; recursive locking/)
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.locked?.should be_false
+    mutex.owned?.should be_false
+    mutex.try_lock.should be_true
+    mutex.unlock if mutex
   end
 
   it "should lock and prevent locking on the same multiple semaphores" do
-    begin
-      mutex = described_class.lock(*@lock_names)
-      mutex.names.should eq @lock_names
-      mutex.locked?.should be_true
-      mutex.owned?.should be_true
-      mutex.should be_an_instance_of described_class
-      described_class.new(*@lock_names).try_lock.should be_false
-      @lock_names.each do |name|
-        described_class.new(name).try_lock.should be_false
-      end
-      mutex.try_lock.should be_false
-      expect {
-        mutex.lock
-      }.to raise_error(Redis::EM::Mutex::MutexError, /deadlock; recursive locking/)
-      @lock_names.each do |name|
-        expect {
-          described_class.new(name).lock
-        }.to raise_error(Redis::EM::Mutex::MutexError, /deadlock; recursive locking/)
-      end
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.locked?.should be_false
-      mutex.owned?.should be_false
-      mutex.try_lock.should be_true
-    ensure
-      mutex.unlock if mutex
+    mutex = described_class.lock(*@lock_names)
+    mutex.names.should eq @lock_names
+    mutex.locked?.should be_true
+    mutex.owned?.should be_true
+    mutex.should be_an_instance_of described_class
+    described_class.new(*@lock_names).try_lock.should be_false
+    @lock_names.each do |name|
+      described_class.new(name).try_lock.should be_false
     end
+    mutex.try_lock.should be_false
+    expect {
+      mutex.lock
+    }.to raise_error(Redis::EM::Mutex::MutexError, /deadlock; recursive locking/)
+    @lock_names.each do |name|
+      expect {
+        described_class.new(name).lock
+      }.to raise_error(Redis::EM::Mutex::MutexError, /deadlock; recursive locking/)
+    end
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.locked?.should be_false
+    mutex.owned?.should be_false
+    mutex.try_lock.should be_true
+    mutex.unlock if mutex
   end
 
   it "should lock and prevent other fibers to lock on the same semaphore" do
-    begin
-      mutex = described_class.lock(@lock_names.first)
-      mutex.should be_an_instance_of described_class
-      mutex.owned?.should be_true
-      locked = true
-      ::EM::Synchrony.next_tick do
-        mutex.try_lock.should be false
+    mutex = described_class.lock(@lock_names.first)
+    mutex.should be_an_instance_of described_class
+    mutex.owned?.should be_true
+    locked = true
+    ::EM::Synchrony.next_tick do
+      begin
+        mutex.try_lock.should be_false
         mutex.owned?.should be_false
         start = Time.now
         mutex.synchronize do
-          (Time.now - start).should be_within(0.01).of(0.26)
-          locked.should be false
+          (Time.now - start).should be_within(0.015).of(0.26)
+          locked.should be_false
           locked = nil
         end
+      rescue Exception => e
+        @exception = e
       end
-      ::EM::Synchrony.sleep 0.25
-      locked = false
-      mutex.owned?.should be_true
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.owned?.should be_false
-      ::EM::Synchrony.sleep 0.1
-      locked.should be_nil
-    ensure
-      mutex.unlock if mutex
     end
+    ::EM::Synchrony.sleep 0.25
+    locked = false
+    mutex.owned?.should be_true
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.owned?.should be_false
+    ::EM::Synchrony.sleep 0.2
+    locked.should be_nil
   end
 
   it "should lock and prevent other fibers to lock on the same multiple semaphores" do
-    begin
-      mutex = described_class.lock(*@lock_names)
-      mutex.should be_an_instance_of described_class
-      mutex.owned?.should be_true
-      locked = true
-      ::EM::Synchrony.next_tick do
-        locked.should be true
-        mutex.try_lock.should be false
+    mutex = described_class.lock(*@lock_names)
+    mutex.should be_an_instance_of described_class
+    mutex.owned?.should be_true
+    locked = true
+    ::EM::Synchrony.next_tick do
+      begin
+        locked.should be_true
+        mutex.try_lock.should be_false
         mutex.owned?.should be_false
         start = Time.now
         mutex.synchronize do
           mutex.owned?.should be_true
-          (Time.now - start).should be_within(0.01).of(0.26)
-          locked.should be false
+          (Time.now - start).should be_within(0.015).of(0.26)
+          locked.should be_false
         end
         mutex.owned?.should be_false
         ::EM::Synchrony.sleep 0.1
         start = Time.now
         ::EM::Synchrony::FiberIterator.new(@lock_names, @lock_names.length).each do |name|
-          locked.should be true
-          described_class.new(name).synchronize do
-            (Time.now - start).should be_within(0.01).of(0.26)
-            locked.should be_an_instance_of Fixnum
-            locked-= 1
+          begin
+            locked.should be_true
+            described_class.new(name).synchronize do
+              (Time.now - start).should be_within(0.015).of(0.26)
+              locked.should be_an_instance_of Fixnum
+              locked-= 1
+            end
+          rescue Exception => e
+            @exception = e
           end
         end
+      rescue Exception => e
+        @exception = e
       end
-      ::EM::Synchrony.sleep 0.25
-      locked = false
-      mutex.owned?.should be_true
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.owned?.should be_false
-      ::EM::Synchrony.sleep 0.1
-
-      locked = true
-      mutex.lock.should be true
-      ::EM::Synchrony.sleep 0.25
-      locked = 10
-      mutex.unlock.should be_an_instance_of described_class
-      ::EM::Synchrony.sleep 0.1
-      locked.should eq 0
-    ensure
-      mutex.unlock if mutex
     end
+    ::EM::Synchrony.sleep 0.25
+    locked = false
+    mutex.owned?.should be_true
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.owned?.should be_false
+    ::EM::Synchrony.sleep 0.1
+
+    locked = true
+    mutex.lock.should be_true
+    ::EM::Synchrony.sleep 0.25
+    locked = 10
+    mutex.unlock.should be_an_instance_of described_class
+    ::EM::Synchrony.sleep 0.1
+    locked.should eq 0
   end
 
   it "should lock and prevent other fibers to lock on the same semaphore with block timeout" do
-    begin
-      mutex = described_class.lock(*@lock_names)
-      mutex.should be_an_instance_of described_class
-      mutex.owned?.should be_true
-      locked = true
-      ::EM::Synchrony.next_tick do
+    mutex = described_class.lock(*@lock_names)
+    mutex.should be_an_instance_of described_class
+    mutex.owned?.should be_true
+    locked = true
+    ::EM::Synchrony.next_tick do
+      begin
         start = Time.now
-        mutex.lock(0.25).should be false
+        mutex.lock(0.25).should be_false
         mutex.owned?.should be_false
         (Time.now - start).should be_within(0.01).of(0.26)
-        locked.should be true
+        locked.should be_true
         locked = nil
+      rescue Exception => e
+        @exception = e
       end
-      ::EM::Synchrony.sleep 0.26
-      locked.should be_nil
-      locked = false
-      mutex.locked?.should be_true
-      mutex.owned?.should be_true
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.locked?.should be_false
-      mutex.owned?.should be_false
-    ensure
-      mutex.unlock if mutex
     end
+    ::EM::Synchrony.sleep 0.26
+    locked.should be_nil
+    locked = false
+    mutex.locked?.should be_true
+    mutex.owned?.should be_true
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.locked?.should be_false
+    mutex.owned?.should be_false
   end
 
   it "should lock and expire while other fiber lock on the same semaphore with block timeout" do
-    begin
-      mutex = described_class.lock(*@lock_names, expire: 0.2499999)
-      mutex.expire_timeout.should eq 0.2499999
-      mutex.should be_an_instance_of described_class
-      mutex.owned?.should be_true
-      locked = true
-      ::EM::Synchrony.next_tick do
+    mutex = described_class.lock(*@lock_names, expire: 0.2499999)
+    mutex.expire_timeout.should eq 0.2499999
+    mutex.should be_an_instance_of described_class
+    mutex.owned?.should be_true
+    locked = true
+    ::EM::Synchrony.next_tick do
+      begin
         mutex.owned?.should be_false
         start = Time.now
-        mutex.lock(0.25).should be true
+        mutex.lock(0.25).should be_true
         (Time.now - start).should be_within(0.011).of(0.26)
-        locked.should be true
+        locked.should be_true
         locked = nil
         mutex.locked?.should be_true
         mutex.owned?.should be_true
@@ -179,52 +177,52 @@ describe Redis::EM::Mutex do
         mutex.unlock.should be_an_instance_of described_class
         mutex.owned?.should be_false
         mutex.locked?.should be_false
+      rescue Exception => e
+        @exception = e
       end
-      ::EM::Synchrony.sleep 0.26
-      locked.should be_nil
-      locked = false
-      mutex.locked?.should be_true
-      mutex.owned?.should be_false
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.locked?.should be_true
-      mutex.owned?.should be_false
-      ::EM::Synchrony.sleep 0.2
-    ensure
-      mutex.unlock if mutex
     end
+    ::EM::Synchrony.sleep 0.26
+    locked.should be_nil
+    locked = false
+    mutex.locked?.should be_true
+    mutex.owned?.should be_false
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.locked?.should be_true
+    mutex.owned?.should be_false
+    ::EM::Synchrony.sleep 0.5
   end
 
   it "should lock and prevent (with refresh) other fibers to lock on the same semaphore with block timeout" do
-    begin
-      mutex = described_class.lock(*@lock_names, expire: 0.11)
-      mutex.should be_an_instance_of described_class
-      mutex.owned?.should be_true
-      locked = true
-      ::EM::Synchrony.next_tick do
+    mutex = described_class.lock(*@lock_names, expire: 0.11)
+    mutex.should be_an_instance_of described_class
+    mutex.owned?.should be_true
+    locked = true
+    ::EM::Synchrony.next_tick do
+      begin
         start = Time.now
-        mutex.lock(0.3).should be false
+        mutex.lock(0.3).should be_false
         mutex.owned?.should be_false
         (Time.now - start).should be_within(0.01).of(0.31)
-        locked.should be true
+        locked.should be_true
         locked = nil
+      rescue Exception => e
+        @exception = e
       end
-      ::EM::Synchrony.sleep 0.08
-      mutex.owned?.should be_true
-      mutex.refresh
-      ::EM::Synchrony.sleep 0.08
-      mutex.owned?.should be_true
-      mutex.refresh(0.5)
-      ::EM::Synchrony.sleep 0.15
-      locked.should be_nil
-      locked = false
-      mutex.locked?.should be_true
-      mutex.owned?.should be_true
-      mutex.unlock.should be_an_instance_of described_class
-      mutex.locked?.should be_false
-      mutex.owned?.should be_false
-    ensure
-      mutex.unlock if mutex
     end
+    ::EM::Synchrony.sleep 0.08
+    mutex.owned?.should be_true
+    mutex.refresh
+    ::EM::Synchrony.sleep 0.08
+    mutex.owned?.should be_true
+    mutex.refresh(0.5)
+    ::EM::Synchrony.sleep 0.15
+    locked.should be_nil
+    locked = false
+    mutex.locked?.should be_true
+    mutex.owned?.should be_true
+    mutex.unlock.should be_an_instance_of described_class
+    mutex.locked?.should be_false
+    mutex.owned?.should be_false
   end
 
   it "should lock some resource and play with it safely" do
@@ -232,20 +230,24 @@ describe Redis::EM::Mutex do
     play_name = SecureRandom.random_bytes
     result = []
     ::EM::Synchrony::FiberIterator.new((0..9).to_a, 10).each do |i|
-      was_locked = false
-      redis = Redis.new @redis_options
-      mutex.owned?.should be_false
-      mutex.synchronize do
-        mutex.owned?.should be_true
-        was_locked = true
-        redis.setnx(play_name, i).should be_true
-        ::EM::Synchrony.sleep 0.1
-        redis.get(play_name).should eq i.to_s
-        redis.del(play_name).should eq 1
+      begin
+        was_locked = false
+        redis = Redis.new @redis_options
+        mutex.owned?.should be_false
+        mutex.synchronize do
+          mutex.owned?.should be_true
+          was_locked = true
+          redis.setnx(play_name, i).should be_true
+          ::EM::Synchrony.sleep 0.1
+          redis.get(play_name).should eq i.to_s
+          redis.del(play_name).should eq 1
+        end
+        was_locked.should be_true
+        mutex.owned?.should be_false
+        result << i
+      rescue Exception => e
+        @exception = e
       end
-      was_locked.should be_true
-      mutex.owned?.should be_false
-      result << i
     end
     mutex.locked?.should be_false
     result.sort.should eq (0..9).to_a
@@ -256,14 +258,18 @@ describe Redis::EM::Mutex do
     mutex.should be_an_instance_of described_class
     time = nil
     EM::Synchrony.next_tick do
-      time.should be_nil
-      was_locked = false
-      mutex.synchronize do
-        time.should be_an_instance_of Time
-        (Time.now - time).should be < 0.0009
-        was_locked = true
+      begin
+        time.should be_nil
+        was_locked = false
+        mutex.synchronize do
+          time.should be_an_instance_of Time
+          (Time.now - time).should be < 0.0009
+          was_locked = true
+        end
+        was_locked.should be_true
+      rescue Exception => e
+        @exception = e
       end
-      was_locked.should be_true
     end
     EM::Synchrony.sleep 0.1
     mutex.owned?.should be_true
@@ -312,11 +318,16 @@ describe Redis::EM::Mutex do
 
   around(:each) do |testcase|
     @after_em_stop = nil
+    @exception = nil
     ::EM.synchrony do
       begin
         testcase.call
-      ensure
+        raise @exception if @exception
         described_class.stop_watcher(false)
+      rescue => e
+        described_class.stop_watcher(true)
+        raise e
+      ensure
         ::EM.stop
       end
     end
