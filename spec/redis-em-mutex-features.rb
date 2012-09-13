e79a9a66402e0607404600cd0903e0f4551c6ce4
Rafał Michalski <royaltm75@gmail.com>
2012-09-13 07:10:18 +0200

    spec: more thorough setup features test

--- spec/redis-em-mutex-features.rb
+++ spec/redis-em-mutex-features.rb
@@ -20,15 +20,64 @@ describe Redis::EM::Mutex do
   end
 
   it "should setup with redis connection pool" do
-    redis_pool = EM::Synchrony::ConnectionPool.new(size: 10) { Redis.new url: 'redis://localhost/1' }
-    described_class.setup(redis: redis_pool)
-    described_class.class_variable_get(:'@@redis_pool').should be redis_pool
+    described_class.setup(redis: @redis_pool)
+    described_class.class_variable_get(:'@@redis_pool').should be @redis_pool
     redis = described_class.class_variable_get(:'@@redis_watcher')
     described_class.stop_watcher
     redis.should be_an_instance_of Redis
     redis.client.host.should eq 'localhost'
     redis.client.db.should eq 1
     redis.client.scheme.should eq 'redis'
+  end
+
+  it "should setup with various options" do
+    described_class.setup do |cfg|
+      cfg.expire = 42 #   - sets global Mutex.default_expire 
+      cfg.ns = 'redis rulez!' #       - sets global Mutex.namespace
+      cfg.reconnect_max = -1 # - maximum num. of attempts to re-establish
+      cfg.url = 'redis://127.0.0.1/2'
+      cfg.size = 10
+    end
+    described_class.namespace.should eq 'redis rulez!'
+    described_class.default_expire.should eq 42
+    described_class.class_variable_get(:'@@connection_retry_max').should eq -1
+    redis_pool = described_class.class_variable_get(:'@@redis_pool')
+    redis_pool.should be_an_instance_of EM::Synchrony::ConnectionPool
+    redis_pool.should_not be @redis_pool
+    redis_pool.client.host.should eq '127.0.0.1'
+    redis_pool.client.db.should eq 2
+    redis_pool.client.port.should eq 6379
+    redis_pool.client.scheme.should eq 'redis'
+    redis = described_class.class_variable_get(:'@@redis_watcher')
+    described_class.stop_watcher
+    redis.should be_an_instance_of Redis
+    redis.client.host.should eq '127.0.0.1'
+    redis.client.db.should eq 2
+    redis.client.port.should eq 6379
+    redis.client.scheme.should eq 'redis'
+  end
+
+  it "should setup with separate redis options" do
+    described_class.setup do |cfg|
+      cfg.scheme = 'redis'
+      cfg.host = 'localhost'
+      cfg.port = 6379
+      cfg.db = 3
+    end
+    redis_pool = described_class.class_variable_get(:'@@redis_pool')
+    redis_pool.should be_an_instance_of EM::Synchrony::ConnectionPool
+    redis_pool.should_not be @redis_pool
+    redis_pool.client.host.should eq 'localhost'
+    redis_pool.client.db.should eq 3
+    redis_pool.client.port.should eq 6379
+    redis_pool.client.scheme.should eq 'redis'
+    redis = described_class.class_variable_get(:'@@redis_watcher')
+    redis.should be_an_instance_of Redis
+    described_class.stop_watcher
+    redis.client.host.should eq 'localhost'
+    redis.client.db.should eq 3
+    redis.client.port.should eq 6379
+    redis.client.scheme.should eq 'redis'  
   end
 
   around(:each) do |testcase|
@@ -43,4 +92,7 @@ describe Redis::EM::Mutex do
     @after_em_stop.call if @after_em_stop
   end
 
+  before(:all) do
+    @redis_pool = EM::Synchrony::ConnectionPool.new(size: 10) { Redis.new url: 'redis://localhost/1' }
+  end
 end
