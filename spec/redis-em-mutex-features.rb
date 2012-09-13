9ff6e7ae42777a555e168496f28d56e50dbd7491
Rafał Michalski <royaltm75@gmail.com>
2012-09-13 06:40:55 +0200

    spec: features setup with :redis

--- spec/redis-em-mutex-features.rb
+++ spec/redis-em-mutex-features.rb
@@ -1,5 +1,6 @@
 $:.unshift "lib"
 require 'em-synchrony'
+require 'em-synchrony/connection_pool'
 require 'redis-em-mutex'
 
 describe Redis::EM::Mutex do
@@ -16,6 +17,18 @@ describe Redis::EM::Mutex do
     expect {
       described_class.setup(port: 65535, reconnect_max: 0)
     }.to raise_error(described_class::MutexError, /Can not establish watcher channel connection!/)
+  end
+
+  it "should setup with redis connection pool" do
+    redis_pool = EM::Synchrony::ConnectionPool.new(size: 10) { Redis.new url: 'redis://localhost/1' }
+    described_class.setup(redis: redis_pool)
+    described_class.class_variable_get(:'@@redis_pool').should be redis_pool
+    redis = described_class.class_variable_get(:'@@redis_watcher')
+    described_class.stop_watcher
+    redis.should be_an_instance_of Redis
+    redis.client.host.should eq 'localhost'
+    redis.client.db.should eq 1
+    redis.client.scheme.should eq 'redis'
   end
 
   around(:each) do |testcase|
