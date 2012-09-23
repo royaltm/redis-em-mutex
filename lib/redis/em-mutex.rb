2f5f725a5e597ee48f3300deb4ff61dbdc9d5151
Rafał Michalski <royaltm75@gmail.com>
2012-09-23 21:52:57 +0200

    Redis::EM::Mutex:NS in autoloaded file

--- lib/redis/em-mutex.rb
+++ lib/redis/em-mutex.rb
@@ -22,6 +22,7 @@ class Redis
     #
     class Mutex
 
+      autoload :NS, 'redis/em-mutex/ns.rb'
       autoload :Macro, 'redis/em-mutex/macro'
 
       module Errors
@@ -58,48 +59,6 @@ class Redis
 
       def block_timeout=(value)
         @block_timeout = value.nil? ? nil : value.to_f
-      end
-
-      class NS
-        attr_reader :ns
-        alias_method :namespace, :ns
-        # Creates a new namespace (Mutex factory).
-        #
-        # - ns = namespace
-        # - opts = options hash:
-        # - :block - default block timeout
-        # - :expire - default expire timeout
-        def initialize(ns, opts = {})
-          @ns = ns
-          @opts = (opts || {}).merge(:ns => ns)
-        end
-
-        # Creates a namespaced cross machine/process/fiber semaphore.
-        #
-        # for arguments see: Redis::EM::Mutex.new
-        def new(*args)
-          if args.last.kind_of?(Hash)
-            args[-1] = @opts.merge(args.last)
-          else
-            args.push @opts
-          end
-          Redis::EM::Mutex.new(*args)
-        end
-
-        # Attempts to grab the lock and waits if it isn’t available.
-        # 
-        # See: Redis::EM::Mutex.lock
-        def lock(*args)
-          mutex = new(*args)
-          mutex if mutex.lock
-        end
-
-        # Executes block of code protected with namespaced semaphore.
-        #
-        # See: Redis::EM::Mutex.synchronize
-        def synchronize(*args, &block)
-          new(*args).synchronize(&block)
-        end
       end
 
       # Creates a new cross machine/process/fiber semaphore
